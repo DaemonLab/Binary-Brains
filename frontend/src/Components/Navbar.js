1bb2b1fc46bbf063519aecf0248a46f8adf61556
exodus31 <sarthak.nandre31@gmail.com>
2022-08-11 21:12:19 +0530

    frontend changes

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -7,7 +7,10 @@ function Navbar() {
   return (        
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-            <Link to="/"><img src={img1} className="navimg"/></Link>                          
+            <Link to="/"><img src={img1} className="navimg"/></Link>     
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>                     
             <div className="collapse navbar-collapse" id="navbarSupportedContent">                
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
@@ -17,19 +20,20 @@ function Navbar() {
                         <Link to="/contests" className="nav-link">Contests</Link>
                     </li>                    
                 </ul>                     
+                <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link to="/yourprofile" className="nav-link">Profile</Link>
+                        </li>
+                        <li className="nav-item active">
+                            <Link to="/login" className="nav-link">Login</Link>
+                        </li>
+                        <li className="nav-item active">
+                            <Link to="/signup" className="nav-link">SignUp</Link>
+                        </li>            
+                    </ul>
             </div>
-            <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-                <Link to="/yourprofile" className="nav-link">Your Profile</Link>
-            </li>
-            <li className="nav-item active">
-                <Link to="/login" className="nav-link">Login</Link>
-            </li>
-            <li className="nav-item active">
-                <Link to="/signup" className="nav-link">SignUp</Link>
-            </li>
-            </ul>
-        </div>  
+            
+            </div>        
     </nav>
 
 
--- frontend/src/Components/Rankings.js
+++ frontend/src/Components/Rankings.js
@@ -24,25 +24,36 @@ function Rankings() {
     <div className='container contestcont'>
       <div className="card contestcard" >       
         <div className="card-body"> 
-          <h2 className="card-title">Contests And Rankings</h2>     
+          <h2 className="card-title">Upcoming Contests</h2>     
           <hr />
           <div className="">            
             <div className="card-body contestbody" key={1}>
               <h3 className='contestname'>{` Contest ${contests[0].id} - ${contests[0].name}`}</h3>                            
-              <p className='contestrank'>1st - {contests[0].rank1} <br/> 2nd - {contests[0].rank2} <br/> 3rd - {contests[0].rank3}</p>                                                                                                      
+              <h4 className='sm'>{contests[0].date}</h4>
+              <button className="btncont">CF Page</button>
             </div>                                    
             <div className="card-body contestbody" key={2}>
               <h3 className='contestname'>{` Contest ${contests[1].id} - ${contests[1].name}`}</h3>                            
-              <p className='contestrank'>1st - {contests[1].rank1} <br/> 2nd - {contests[1].rank2} <br/> 3rd - {contests[1].rank3}</p>                                                                                                      
+              <h4 className='sm'>{contests[1].date}</h4>
+              <button className="btncont">CF Page</button>
             </div>                                    
-          </div>                   
+            <div className="card-body contestbody" key={3}>
+              <h3 className='contestname'>{` Contest ${contests[2].id} - ${contests[2].name}`}</h3>                            
+              <h4 className='sm'>{contests[2].date}</h4>
+              <button className="btncont">CF Page</button>
+            </div>
+          </div>                             
+        </div>
+        <div className="container">
+          <hr/>
         </div>
         <Link to="contests"><button className='btnmore' >View All</button></Link>
       </div>    
-      <div className="card contestcard-2" >       
-      <div className="container">
+      <div className="card contestcard-2 col-sm-12" >       
+      <div className="container ">
       <h2 className="card-title contesttitle-2">Leaderboard</h2>
       <hr/>
+      <h3 align="center">Beginner</h3>
       <div className="responsive">
         <table className='leaderboard'>
           <tbody>
@@ -64,10 +75,34 @@ function Rankings() {
             }     
           </tbody>   
         </table>
+        <h1></h1>
+        </div>
+        <h3 align="center">Advanced</h3>        
+        <div className="responsive leaderboard">
+          <table>
+            <tbody>
+              <tr>
+                <th className='th'>Name</th>
+                <th className='th'>Username</th>
+                <th className='th'>Points</th>
+              </tr>
+              {
+                ranks.map((rank, id) => {
+                  return(
+                    <tr key={id}>
+                      <td className='td'>{rank.name}</td>
+                      <td className='td'>{rank.username}</td>
+                      <td className='td'>{rank.points}</td>
+                    </tr>
+                  )
+                })
+              }     
+            </tbody>   
+          </table>        
         </div>
       </div>
       </div>
-    </div>  
+    </div>   
   
   )
 }
