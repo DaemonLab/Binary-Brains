57f2454ab77885a1284c19de5e291fc71de39c69
exodus31 <sarthak.nandre31@gmail.com>
2022-08-21 14:46:34 +0530

    poster changed

--- frontend/src/Components/Poster.js
+++ frontend/src/Components/Poster.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import img1 from './BBposter.png'
+import img1 from './BBposterx.png'
+import img2 from './logox.png'
 import { Link } from 'react-router-dom'
 
 function Poster() {
