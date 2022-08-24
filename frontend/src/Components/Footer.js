bf7b52da410584d5f6e0ac1416b28dc98de24678
exodus31 <sarthak.nandre31@gmail.com>
2022-08-24 15:54:45 +0530

    bugs

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -2,17 +2,6 @@ import React from 'react'
 
 function Footer() {
   return (
-    // <div>lauda</div>
-    
-    // // <div className='footer'>
-//         <div className="container">
-//         <h2 className="thanksNote" align="center">Thanks For Visiting!</h2>
-//         <button className='btninsta' href="https://www.instagram.com/pclub_iiti/"><h5><a href="https://www.instagram.com/pclub_iiti/" className='instalink' target="_blank">Instagram</a></h5></button>
-//         <button className='btnfb' href="https://www.facebook.com/pclubiiti/" ><h5><a href="https://www.facebook.com/pclubiiti/" className='discord' target="_blank">Facebook</a></h5></button>
-//         <h6 className='contact'>Contact Details : Jaskaran Singh - <span className="small">8872150472</span></h6>
-//         <br/>                                                          
-//     </div>
-// </div>
     <footer className="text-center text-white">
       <h2 className="thanksNote" align="center">Thanks For Visiting!</h2>
       <h6 className='contact'>Contact Details : Jaskaran Singh - <span className="small">8872150472</span></h6>
