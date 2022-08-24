7490b55be53f08d65012aeda5bbb97b2184207ad
exodus31 <sarthak.nandre31@gmail.com>
2022-08-24 15:49:46 +0530

    spelling changes

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -5,8 +5,8 @@ function Footer() {
     <div className='footer'>
         <div className="container">
             <h2 className="thanksNote" align="center">Thanks For Visiting!</h2>
-            <button className='btninsta'><h5><a href="https://www.instagram.com/pclub_iiti/" className='instalink' target="_blank">Instagram</a></h5></button>
-            <button className='btnfb'><h5><a href="https://www.facebook.com/pclubiiti/" className='discord' target="_blank">Facebook</a></h5></button>
+            <button className='btninsta' href="https://www.instagram.com/pclub_iiti/"><h5><a href="https://www.instagram.com/pclub_iiti/" className='instalink' target="_blank">Instagram</a></h5></button>
+            <button className='btnfb' href="https://www.facebook.com/pclubiiti/" ><h5><a href="https://www.facebook.com/pclubiiti/" className='discord' target="_blank">Facebook</a></h5></button>
             <h6 className='contact'>Contact Details : Jaskaran Singh - <span className="small">8872150472</span></h6>
             <br/>                                                          
         </div>
